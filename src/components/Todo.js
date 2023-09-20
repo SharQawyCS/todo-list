17763f4d6a75eebf7fdb556bff5cc737da298153
SharQawyCS <sharqawycs@gmail.com>
2023-09-20 07:28:18 +0300

    Add Card

---
+++ src/components/Todo.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+
+export default function Todo() {
+  return (
+    <>
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent></CardContent>
+        <CardActions></CardActions>
+      </Card>
+    </>
+  );
+}
